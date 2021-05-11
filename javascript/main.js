efe8f9ef85e4d733238a334cc174bf1d49f757a5
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-05-11 17:08:12 +0300

    minor changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,77 +1,73 @@
-
-
-document.querySelector(".js-go").addEventListener('click', function(){
-
-    var input = document.querySelector("input").value;
-    pushToDOM(input);
-    
-
-    
-});
-
-
-// function içerisinde tanımladıgın değişkenleri dışarda da kullanabilmek için  pushToDOM(input); kullanmak gerekiyor
-
-document.querySelector(".js-userinput").addEventListener('keyup', function(e) {
-
-    var input = document.querySelector("input").value;
-
-
-    // 13 means enter
-
-    if(e.which === 13) {
-
-        pushToDOM(input);
-    };
-
-    
-
-
+document.querySelector(".js-go").addEventListener('click', function() {
+	var inputValue = document.querySelector('.js-userinput').value;
+		var userInput = getUserInput();
+	searchGiphy( userInput );
 
 });
 
-// giphy api key + limit
-var url = "https://api.giphy.com/v1/gifs/search?q=dogs&api_key=gmrPn3D2KvMWNb6PryhYtjdbxAI2y3Np&limit=20&q=";
-
-var GiphAJAXCall = new XMLHttpRequest();
-GiphAJAXCall.open('GET', url);
-GiphAJAXCall.send();
-
-
-GiphAJAXCall.addEventListener('load',function(e){
-
-   var data = e.target.response;
-   pushToDOM(data);
-
+document.querySelector('.js-userinput').addEventListener('keyup', function (e) {
+	if (e.which === 13) {
+ 		var userInput = getUserInput();
+ 		searchGiphy( userInput );
+    }
 });
 
-// 
+function getUserInput() {
+	var inputValue = document.querySelector('.js-userinput').value;
 
-
-function pushToDOM(input) {
-
-
-    var response = JSON.parse(input);
-    var imageUrls = response.data;
-
-    // var imageUrl = response.data[0].images.fixed_height.url; // json path. also for just one image
-
-
-    imageUrls.forEach(function(image){  // loop
-    
-    var src = image.images.fixed_height.url;
-
-
-    var container = document.querySelector(".js-container");
-    container.innerHTML += "<img src="  + src + "\" class=\"container-image\">";
+	return inputValue;
+}
 
 
 
-    });
 
-  
 
-   
+function searchGiphy( searchQuery ) {
+	var url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchQuery;
+
+	// AJAX Request
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open( 'GET', url );
+	GiphyAJAXCall.send();
+
+
+	GiphyAJAXCall.addEventListener('load', function( data ) {
+
+			var actualData = data.target.response;
+			pushToDOM(actualData);
+			console.log(actualData);
+		
+	});
 
 }
 
+
+
+
+
+
+
+
+
+
+function pushToDOM( response ) {
+	// turn response into real javascript object
+	response = JSON.parse( response );
+	// drill down to the data array
+	var images = response.data;
+
+	// find the container to hold this stuff in DOM
+	var container = document.querySelector('.js-container');
+	// clear it of old content since this function will be used on every search
+	// we want to reset the div
+	container.innerHTML = "";
+
+	// loop through data array and add IMG html
+	images.forEach(function(image){
+		// find img src
+		var src = image.images.fixed_height.url;
+
+		// concatenate a new IMG tag
+		container.innerHTML += "<img src='"+ src +"' class='container-image' />";
+	});
+}
